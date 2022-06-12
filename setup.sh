775e3417b109539b3e1f8e516ae9f815e765be52
thenewtonium <60141408+thenewtonium@users.noreply.github.com>
2022-06-12 20:47:57 +0100

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd $BASH_SOURCE
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16.15.1
+nvm use 16.15.1
+yarn global add pm2
+pm2 start index.js --name MadLibs
+pm2 save
